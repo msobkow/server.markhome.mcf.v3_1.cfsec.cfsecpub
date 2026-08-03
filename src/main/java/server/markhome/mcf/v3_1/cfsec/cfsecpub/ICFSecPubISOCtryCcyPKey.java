6eb3b337d843126e9234f388e7b09d7a393bec02
Mark Sobkow <mark.sobkow@gmail.com>
2026-08-03 10:37:46 -0600

    2026-08-03 10:35 CST A tremendous amount of work has been done to reference the proper versions of Min, Max, Init, and MaxLen values to validate getters and setters. There are still some errors in the resulting code, but it looks pretty good at this point save for those few exceptions which cause some files to be left empty and others to have bad source code syntax.

--- src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecpub/ICFSecPubISOCtryCcyPKey.java
+++ src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecpub/ICFSecPubISOCtryCcyPKey.java
@@ -56,7 +56,7 @@ public interface ICFSecPubISOCtryCcyPKey
 	public int hashCode();
 
 	//@Override
-	public int compareTo( ICFSecPubISOCtryCcyPKey rhs );
+	public int compareTo( Object rhs );
 
 	public String getXmlAttrFragment();
 
--- src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecpub/ICFSecPubISOCtryLangPKey.java
+++ src/main/java/server/markhome/mcf/v3_1/cfsec/cfsecpub/ICFSecPubISOCtryLangPKey.java
@@ -56,7 +56,7 @@ public interface ICFSecPubISOCtryLangPKey
 	public int hashCode();
 
 	//@Override
-	public int compareTo( ICFSecPubISOCtryLangPKey rhs );
+	public int compareTo( Object rhs );
 
 	public String getXmlAttrFragment();
 
